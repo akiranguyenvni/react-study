8a31bc0eb9e853eae391eab95887071dfb992b74
akiranguyenvni <akiranguyen.vni@gmail.com>
2022-09-22 07:18:08 +0700

    dev - complete: coding live chat

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChatBox from "../components/ChatBox";
+
+export default function HomePage() {
+    return (
+        <div>
+            <div>HomePage</div>
+
+            <p>Your website content</p>
+            <ChatBox />
+        </div>
+    )
+}
